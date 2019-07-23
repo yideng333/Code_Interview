3ddb40992d5a751eb095887c1109be4f263cb139
Deng Yi <yideng@cashbus.com>
2019-07-23 15:20:57 +0800

    update 02_singleton

--- Code_interview/02_singleton.py
+++ Code_interview/02_singleton.py
@@ -5,7 +5,7 @@ import threading
 from functools import wraps
 
 
-# __new__方法实现，init函数每次初始化都会执行
+# __new__方法实现 (init函数每次实例化都会执行)
 class Singleton_1(object):
     # _instance = None
 
@@ -30,10 +30,23 @@ class Singleton_2(object):
         return cls._instance
 
 
-# 元类实现，init函数只会执行一次
+# 元类实现 (init函数只会执行一次)
+"""
+1.类由type创建，创建类时，type的__init__方法自动执行，类() 执行type的 __call__方法(类的__new__方法,类的__init__方法)
+2.对象由类创建，创建对象时，类的__init__方法自动执行，对象()执行类的 __call__ 方法
+"""
 class Singleton_3(type):
     _instances = {}
 
+    def __new__(cls, *args, **kwargs):
+        print("Singleton_3 __new__")
+        return super(Singleton_3, cls).__new__(cls, *args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        print('Singleton_3 __init__')
+        super(Singleton_3, self).__init__(*args, **kwargs)
+
+    # 实现类实例的创建, 依次调用类对象的__new__方法和__init__方法
     def __call__(cls, *args, **kwargs):
         print('Singleton_3 __call__')
         if cls not in cls._instances:
@@ -62,13 +75,23 @@ class Foo(metaclass=Singleton_3):
         return super(Foo, cls).__new__(cls)
 
 
+class Foo_1(Singleton_1):
+    def __init__(self):
+        print('Foo __init__')
+
+    def __new__(cls):
+        print("Foo __new__")
+        return super(Foo_1, cls).__new__(cls)
+
+
 @singleton
-class Foo_1():
+class Foo_2():
     def __init__(self):
         print('Foo __init__')
 
 
 if __name__ == '__main__':
+    print('-' * 20)
     obj1 = Foo()
     print('-' * 20)
     obj2 = Foo()
